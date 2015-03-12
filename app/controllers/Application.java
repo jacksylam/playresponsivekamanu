1e471feeae0c1c2332442ca9cd2a70d945f0ab2b
Jack Lam <catinacan@gmail.com>
2015-03-11 14:00:15 -1000

    Finished adding all three extra pages.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,7 +2,10 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.About;
+import views.html.Aukahi;
 import views.html.Index;
+import views.html.Kamanu;
 import views.html.Pueo;
 
 /**
@@ -25,6 +28,30 @@ public class Application extends Controller {
    */
   public static Result pueo() {
     return ok(Pueo.render("Welcome to the Pueo page."));
+  }
+
+  /**
+   * Returns the Aukahi page.
+   * @return The resulting Aukahi page.
+   */
+  public static Result aukahi() {
+    return ok(Aukahi.render("Welcome to the Aukahi page."));
+  }
+
+  /**
+   * Returns the Kamanu page.
+   * @return The resulting Kamanu page.
+   */
+  public static Result kamanu() {
+    return ok(Kamanu.render("Welcome to the Kamanu page."));
+  }
+
+  /**
+   * Returns the About page.
+   * @return The resulting About page.
+   */
+  public static Result about() {
+    return ok(About.render("Welcome to the About page."));
   }
 
 
--- test/test/IntegrationTest.java
+++ test/test/IntegrationTest.java
@@ -29,6 +29,15 @@ public class IntegrationTest {
         browser.goTo("http://localhost:3333/pueo");
         assertThat(browser.pageSource()).contains("The Pueo is the most versatile");
 
+        browser.goTo("http://localhost:3333/aukahi");
+        assertThat(browser.pageSource()).contains("The Aukahi: Tahitian V1 design");
+
+        browser.goTo("http://localhost:3333/kamanu");
+        assertThat(browser.pageSource()).contains("The most exciting thing in paddling since");
+
+        browser.goTo("http://localhost:3333/about");
+        assertThat(browser.pageSource()).contains("Become the most amazing company");
+
       }
     });
   }
